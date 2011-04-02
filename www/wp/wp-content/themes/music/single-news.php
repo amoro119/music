37efe2c940629499ff1554c74bb4cfdd207b4e2b
eric <eric@xtunes.cc>
2011-04-03 02:40:59 +0800

    更新数据库

--- www/wp/wp-content/themes/music/single-news.php
+++ www/wp/wp-content/themes/music/single-news.php
@@ -2,27 +2,7 @@
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<title><?php
-	/*
-	 * Print the <title> tag based on what is being viewed.
-	 */
-	global $page, $paged;
-
-	wp_title( '|', true, 'right' );
-
-	// Add the blog name.
-	bloginfo( 'name' );
-
-	// Add the blog description for the home/front page.
-	$site_description = get_bloginfo( 'description', 'display' );
-	if ( $site_description && ( is_home() || is_front_page() ) )
-		echo " | $site_description";
-
-	// Add a page number if necessary:
-	if ( $paged >= 2 || $page >= 2 )
-		echo ' | ' . sprintf( __( 'Page %s', 'twentyten' ), max( $paged, $page ) );
-
-	?></title>
+<title></title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <link rel="stylesheet" href="/style.css" type="text/css" media="screen" charset="utf-8" />
 <meta name="keywords" content="keywords" />
